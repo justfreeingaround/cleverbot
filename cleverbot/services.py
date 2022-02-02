2fce2ce959bd9304d1e6f4aaa7ea574875907ca8
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-02-02 19:14:13 +0545

    Chore: Services update

--- cleverbot/services.py
+++ cleverbot/services.py
@@ -7,8 +7,8 @@ Using multiple clients will strengthen your reliability.
 SERVICES = {
     'cleverbot-official': ('https://www.cleverbot.com/', 'webservicemin'),
     'boibot': ('https://www.boibot.com/', 'webservicemin'),
-    'polibot': ('https://www.polibot.com/', 'webservicemin'),
+    'politbot': ('https://www.politbot.com/', 'webservicemin'),
     'pewdiebot': ('https://www.pewdiebot.com/', 'webservicemin'),
-    'eviebot': ('https://www.eeviebot.com/', 'webservicemin'),
+    'eviebot': ('https://www.eviebot.com/', 'webservicemin'),
     'thoughtscreen': ('https://www.thoughtscreen.com/', 'webservicemin'),
 }
